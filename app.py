34a65817810ef4d9562868e176a9a3e07cb0af3e
gtaneja1 <gtaneja1@asu.edu>
2026-05-22 20:07:15 -0700

    updating index.html and app.py code

--- app.py
+++ app.py
@@ -7,9 +7,15 @@
 # ========================================================================
 
 import os
-from flask import Flask, jsonify, request, send_from_directory
-from flask_cors import CORS
-from dotenv import load_dotenv
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 # Load environment variables from .env
 load_dotenv()
